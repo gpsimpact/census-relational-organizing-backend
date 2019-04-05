34703b03183f07916b99e43042c7e446949387b6
jakelowen <jakelowen@gmail.com>
2019-04-05 10:59:18 -0500

    Fixes slug case coercion issues

--- src/middleware/createSlug/createSlug.js
+++ src/middleware/createSlug/createSlug.js
@@ -3,7 +3,9 @@ import slugify from "slugify";
 export default async (resolve, root, args, context, info) => {
   const transformedArgs = Object.assign({}, args);
   if (transformedArgs && transformedArgs.input && transformedArgs.input.name) {
-    transformedArgs.input.slug = slugify(transformedArgs.input.name);
+    transformedArgs.input.slug = slugify(
+      transformedArgs.input.name.toLowerCase()
+    );
   }
   return resolve(root, transformedArgs, context, info);
 };
