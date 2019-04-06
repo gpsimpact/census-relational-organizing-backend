1be70815b9036ccc7a8bf33b8637f1ab7a7a1a72
jakelowen <jakelowen@gmail.com>
2019-04-06 13:26:18 -0500

    deploy debug fix notauthrole

--- src/middleware/permissions/isNotAuthenticated.js
+++ src/middleware/permissions/isNotAuthenticated.js
@@ -3,7 +3,12 @@ import { rule } from "graphql-shield";
 export default rule({ cache: "contextual" })(async (parent, args, ctx) => {
   console.log("USER ID IS", ctx.req.session.userId);
   console.log("USER session IS", ctx.req.session);
-  if (ctx.req.session.userId !== null || ctx.req.session.userId !== undefined) {
+  if (
+    ctx.req.session &&
+    ctx.req.session.userId &&
+    ctx.req.session.userId !== null &&
+    ctx.req.session.userId !== undefined
+  ) {
     return new Error("You are already authenticated");
   }
   return true;
