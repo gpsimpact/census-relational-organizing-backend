cb3ae01455a2a23e5849a3e3f061425c9315f4e9
jakelowen <jakelowen@gmail.com>
2019-04-17 11:08:42 -0500

    supplying a team id on register mutation creates automatic team application permission.

--- src/__tests__/register.js
+++ src/__tests__/register.js
@@ -1,7 +1,8 @@
 import faker from "faker";
 import { graphqlTestCall } from "../utils/graphqlTestCall";
 import { dbUp, dbDown } from "../utils/testDbOps";
-import { createTestUser } from "../utils/createTestEntities";
+import { createTestUser, createTestTeam } from "../utils/createTestEntities";
+import { sq } from "../db";
 
 const REGISTER_MUTATION = `
     mutation register($input: RegisterInput!){
@@ -131,5 +132,48 @@ describe("RegisterResolver", () => {
     });
     expect(res.data.register.code).toBe("DUPLICATE");
     expect(res.data.register.success).toBe(false);
+  });
+
+  test("if optional team id arg is present, creates application record", async () => {
+    const mockSendEmail = jest.fn();
+    const team = await createTestTeam();
+    const email = faker.internet.email().toLowerCase();
+    const response = await graphqlTestCall(
+      REGISTER_MUTATION,
+      {
+        input: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email,
+          address: faker.address.streetAddress(),
+          city: faker.address.city(),
+          state: faker.address.state(),
+          zip5: faker.address.zipCode().substring(0, 5),
+          phone: `+${faker.random.number({
+            min: 10000000000,
+            max: 19999999999
+          })}`,
+          teamId: team.id
+        }
+      },
+      {
+        sendEmail: mockSendEmail
+      }
+    );
+    expect(response.data.register.code).toBe("OK");
+    expect(response.data.register.message).toBe(
+      "A message has been sent to your email with a magic link you need to click to log in."
+    );
+    expect(response.data.register.success).toBe(true);
+
+    // get user from db
+    const dbUser = await sq`users`.where({ email }).one();
+
+    const dbUserPerms = await sq.from`team_permissions`.where({
+      userId: dbUser.id,
+      teamId: team.id
+    });
+    expect(dbUserPerms).toHaveLength(1);
+    expect(dbUserPerms[0].permission).toEqual("APPLICANT");
   });
 });
