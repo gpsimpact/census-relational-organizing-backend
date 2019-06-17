1877379c79ba7573c3e1232398696b0b89928a93
jakelowen <jakelowen@gmail.com>
2019-06-17 16:26:59 -0500

    team admin can run userTargetsQuery by specific userId. Resolves #121.

--- src/__tests__/userTargets.js
+++ src/__tests__/userTargets.js
@@ -9,8 +9,8 @@ import {
 } from "../utils/createTestEntities";
 
 const GET_ALL_USER_TARGETS_QUERY = `
-query userTargets($teamId: String!, $input:TargetsInput) {
-    userTargets(teamId: $teamId, input:$input) {
+query userTargets($teamId: String!, $userId:String, $input:TargetsInput) {
+    userTargets(teamId: $teamId, userId:$userId, input:$input) {
         hasMore
         totalCount
         items {
@@ -107,6 +107,64 @@ describe("User Targetss", () => {
       { user: { id: user.id } }
     );
     debugResponse(response1);
+    expect(response1.data).toBeNull();
+    expect(response1.errors.length).toEqual(1);
+    expect(response1.errors[0].message).toEqual("Not Authorized!");
+  });
+
+  test("team admin can query for member user targets by specifying userId arg", async () => {
+    const teamAdminUser = await createTestUser();
+    const user2 = await createTestUser();
+    const team = await createTestTeam();
+    await createTestTarget({ userId: user2.id, teamId: team.id });
+    await createTestTarget({ userId: user2.id, teamId: team.id });
+    await createTestOLPermission(user2.id, team.id, "MEMBER");
+    await createTestOLPermission(teamAdminUser.id, team.id, "ADMIN");
+
+    // AS TEAM ADMIN
+    const response1 = await graphqlTestCall(
+      GET_ALL_USER_TARGETS_QUERY,
+      { teamId: team.id, userId: user2.id },
+      { user: { id: teamAdminUser.id } }
+    );
+    debugResponse(response1);
+
+    // AS USER
+    const response2 = await graphqlTestCall(
+      GET_ALL_USER_TARGETS_QUERY,
+      { teamId: team.id },
+      { user: { id: user2.id } }
+    );
+    debugResponse(response2);
+
+    expect(response1).toEqual(response2);
+  });
+
+  test("non team admin CAN NOT query for member user targets by specifying userId arg", async () => {
+    const user1 = await createTestUser();
+    const user2 = await createTestUser();
+    const team = await createTestTeam();
+    await createTestTarget({ userId: user2.id, teamId: team.id });
+    await createTestTarget({ userId: user2.id, teamId: team.id });
+    await createTestOLPermission(user2.id, team.id, "MEMBER");
+    await createTestOLPermission(user1.id, team.id, "MEMBER");
+
+    // AS TEAM ADMIN
+    const response1 = await graphqlTestCall(
+      GET_ALL_USER_TARGETS_QUERY,
+      { teamId: team.id, userId: user2.id },
+      { user: { id: user1.id } }
+    );
+    debugResponse(response1);
+
+    // AS USER
+    const response2 = await graphqlTestCall(
+      GET_ALL_USER_TARGETS_QUERY,
+      { teamId: team.id },
+      { user: { id: user2.id } }
+    );
+    debugResponse(response2);
+
     expect(response1.data).toBeNull();
     expect(response1.errors.length).toEqual(1);
     expect(response1.errors[0].message).toEqual("Not Authorized!");
--- src/resolvers/query/userTargets.js
+++ src/resolvers/query/userTargets.js
@@ -2,7 +2,7 @@ import { getManyHOR } from "@jakelowen/sqorn-graphql-filters";
 
 export default async (root, args, context, info) => {
   const dbHandle = context.sq.from`targets`.where({
-    userId: context.user.id,
+    userId: args.userId,
     teamId: args.teamId
   });
   return getManyHOR(dbHandle)(root, args, context, info);
