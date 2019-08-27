a26d589bc784e39ae33328a328aa00aaa7b71e4c
jakelowen <jakelowen@gmail.com>
2019-08-27 11:25:25 -0500

    updateTeam allows set inactive

--- src/__tests__/updateTeam.js
+++ src/__tests__/updateTeam.js
@@ -18,6 +18,7 @@ const UPDATE_TEAM_MUTATION = `
       item {
         id
         name 
+        active
       }
     }
   }
@@ -57,6 +58,31 @@ describe("Create Team", () => {
     expect(dbTeam).toBeDefined();
     expect(dbTeam.name).toEqual(newData.name);
     expect(dbTeam.abbreviation).toEqual(team.abbreviation);
+  });
+
+  test("Admin can set team inactive", async () => {
+    const team = await createTestTeam();
+    const adminUser = await createAdminUser();
+
+    const newData = {
+      active: false
+    };
+
+    const response = await graphqlTestCall(
+      UPDATE_TEAM_MUTATION,
+      {
+        id: team.id,
+        input: newData
+      },
+      { user: { id: adminUser.id } }
+    );
+    expect(response.data.updateTeam).not.toBeNull();
+    expect(response.data.updateTeam.item.active).toEqual(newData.active);
+    expect(response.data.updateTeam.item.name).toEqual(team.name);
+    const [dbTeam] = await sq.from`teams`.where({ id: team.id });
+    expect(dbTeam).toBeDefined();
+    expect(dbTeam.active).toEqual(false);
+    expect(dbTeam.name).toEqual(team.name);
   });
 
   test("Happy Path by Team  Admin", async () => {
