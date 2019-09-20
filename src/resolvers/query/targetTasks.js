7b05661fb075481296f080c1e76244bcd0c4acbf
jakelowen <jakelowen@gmail.com>
2019-09-20 12:53:09 -0500

    adding active filter to targetTasks

--- src/resolvers/query/targetTasks.js
+++ src/resolvers/query/targetTasks.js
@@ -3,9 +3,13 @@ export default async (root, args, context) => {
   const target = await context.dataSource.target.byIdLoader.load(args.targetId);
 
   if (target && target.teamId) {
-    const teamTaskAssignments = await context.dataSource.taskAssignment.byTeamIdLoader.load(
-      target.teamId
-    );
+    // const teamTaskAssignments = await context.dataSource.taskAssignment.byTeamIdLoader.load(
+    //   target.teamId
+    // );
+    const teamTaskAssignments = context.sq`task_assignments`.where({
+      teamId: target.teamId,
+      active: args.active
+    });
 
     return teamTaskAssignments;
   }
