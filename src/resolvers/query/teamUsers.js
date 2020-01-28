062ba57db1fd2a538e4bb6df40f82cb5442e8eb9
jakelowen <jakelowen@gmail.com>
2020-01-28 14:39:12 -0600

    fixing teamUsers query to make includePermissions work.

--- src/resolvers/query/teamUsers.js
+++ src/resolvers/query/teamUsers.js
@@ -1,20 +1,14 @@
 import { getManyHOR } from "@jakelowen/sqorn-graphql-filters";
 
 export default async (root, args, context, info) => {
-  // const perms = makeDefaultState();
-  // args.input.includePermissions.forEach(x => {
-  //   perms[x] = true;
-  // });
-  // const permInt = permsToInt(perms);
-
   const dbHandle = context.sq.from`users`.where(
     context.sq
       .txt`(EXISTS (SELECT user_id FROM team_permissions WHERE users.id = user_id and team_id = ${
       args.input.teamId
-    }))`
+    } and permission IN ${context.sq.txt(
+      ...args.input.includePermissions
+    )} LIMIT 1))`
   );
-
-  // console.log(await dbHandle);
 
   return getManyHOR(dbHandle)(root, args, context, info);
 };
